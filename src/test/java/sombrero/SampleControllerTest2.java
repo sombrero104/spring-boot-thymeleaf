19803ba455f824f6e33ea22e7a308a5362344486
sombrero104 <sombrero.khy@gmail.com>
2019-06-25 20:04:01 +0900

    .

---
+++ src/test/java/sombrero/SampleControllerTest2.java
@@ -0,0 +1,34 @@
+package sombrero;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest2 {
+
+    /**
+     * htmlUnit 의존성 추가 시 WebClient 를 주입받을 수 있음.
+     */
+    @Autowired
+    WebClient webClient;
+
+    @Test
+    public void hello() throws IOException {
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("sombrero");
+    }
+
+}
